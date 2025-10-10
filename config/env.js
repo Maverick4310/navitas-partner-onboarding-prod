f8d90fcf32720ebfde98829b8a862e1e7ea55924
Maverick4310 <cdiaz@navitascredit.com>
2025-10-10 18:29:00 -0400

    Create env.js

---
+++ config/env.js
@@ -0,0 +1,8 @@
+/**
+ * Environment variable helper
+ * (Optional – currently not used but available for future)
+ */
+module.exports = {
+  mode: process.env.MODE,
+  targetUrl: process.env.TARGET_SF_URL
+};
